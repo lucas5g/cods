02754100e7ca6030136ac7cda2dae746c8d88922
lucas.sousa <lucas.sousa@codificar.com.br>
2023-08-16 00:10:02 -0300

    fix src

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { ProjectsModule } from 'src/projects/projects.module';
-import { ClientsModule } from 'src/clients/clients.module';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { ProjectsModule } from './projects/projects.module';
+import { ClientsModule } from './clients/clients.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [ProjectsModule, PrismaModule, ClientsModule],
