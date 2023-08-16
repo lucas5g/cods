06addf822da5d141ea7ff5d10dddd779c892a3fb
lucas.sousa <lucas.sousa@codificar.com.br>
2023-08-16 00:13:55 -0300

    fix client module

--- src/clients/clients.module.ts
+++ src/clients/clients.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ClientsService } from './clients.service';
 import { ClientsController } from './clients.controller';
-import { ExistsValidator } from 'src/validators/exists.validator';
 
 
 @Module({
   controllers: [ClientsController],
-  providers: [ClientsService, ExistsValidator],
+  providers: [ClientsService],
 })
 export class ClientsModule {}
